60ef791045c0059c45086a6e64e23788326f92b1
nvrmuxtorov <nvrmuxtorov@gmail.com>
2024-01-12 15:35:04 +0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,82 @@
+// premetr
+// let a =+prompt("a ni kiriting");
+// P=4*a;
+// console.log(P);
+
+
+// yuz
+// let a =+prompt("a ni kiriting");
+// S=a**2;
+// console.log(S);
+
+// tortburchak
+// let a = +prompt("a ni kiriting  ");
+// let b = +prompt("b ni kiriting");
+// S=a*b;
+// P=2*(a+b);
+// console.log(P,S);
+
+//diametr
+// let d = +prompt("d ni kiriting");
+// L=Math.PI*d;
+// console.log(L);
+
+// kub
+// let a =+prompt("a ni kiriting");
+// V=a**3;
+// S=6*a**2;
+// console.log(V,S);
+
+
+// parallelpiped
+// let a = +prompt("a ni kiriting  ");
+// let b = +prompt("b ni kiriting");
+// let c = +prompt("c ni kiriting");
+// S=2(a*b+a*c +c*a);
+// console.log(P,S);
+
+// Doira
+// let r =+prompt("r ni kiriting");
+// L=2*Math.PI*r
+// S=Math.PI*r**2;
+// console.log(L,S)
+
+// ikkita son
+// let a = +prompt("a ni kiriting  ");
+// let b = +prompt("bni kiriting");
+// Sum=(a+b)/2;
+// console.log(Sum);
+
+// ikkita musbat son
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("bni kiriting");
+// if(a>0&&b>0){
+//     sum=Math.sqrt((a*b),2)
+//     console.log(sum);
+// }else{
+//     console.log("error");
+// }
+
+
+// nolga teng emas
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("bni kiriting");
+// if(a!=0&&b!=0){
+//    sum=a+b;
+//    ayir=Math.abs(a-b);
+//    kop=a*b;
+//    kvad1=a**2
+// kvad2=b**2
+// mod1=Math.abs(a)
+// mod2=Math.abs(b)
+// console.log(sum,ayir,kop,kvad1 ,kvad2 ,mod1 ,mod2);
+// }else{
+//     console.log("error");
+// }
+
+// uchburchak
+// let a = +prompt("a ni kiriting");
+//  let b = +prompt("bni kiriting");
+// c=Math.sqrt(a**2+b**2);
+// P=a+b+c;
+// console.log(c,P);
